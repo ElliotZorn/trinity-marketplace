ba005be996fd7f70fd860a58b7dd0bff2bc408d8
Elliott Zorn <ezorn@trinity.edu>
2025-12-02 20:42:44 -0600

    fixed router error

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ var mongoose = require('mongoose');
 
 const seedDB = require('./scripts/seed/seedDB');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var interestsRouter = require('./routes/interests');
-var purchasesRouter = require('./routes/purchases');
-var productsRouter = require('./routes/products');
+// var usersRouter = require('./routes/users');
+// var interestsRouter = require('./routes/interests');
+// var purchasesRouter = require('./routes/purchases');
+// var productsRouter = require('./routes/products');
 
 
 var app = express();
@@ -40,10 +40,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/interests', interestsRouter);
-app.use('/purchases', purchasesRouter);
-app.use('/products', productsRouter); 
+// app.use('/users', usersRouter);
+// app.use('/interests', interestsRouter);
+// app.use('/purchases', purchasesRouter);
+// app.use('/products', productsRouter); 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
